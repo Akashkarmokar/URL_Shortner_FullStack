d508c48fd599e9d7c9f59dd7da54137c72e747f9
akash <akashcsemu@gmail.com>
2026-01-05 23:49:10 +0600

    feat: Navbar has been modified

---
+++ FrontEnd/src/component/Navbar.tsx
@@ -0,0 +1,39 @@
+"use client";
+import React, { useState } from "react";
+import { Menu } from "lucide-react";
+const Navbar = () => {
+    const [menu, isMenuOpen] = useState(false);
+
+    const handleMenu = () => {
+        console.log("clicked");
+        isMenuOpen(!menu);
+    };
+    return (
+        <nav className="mx-auto flex w-[92%] items-center justify-end py-6 text-white">
+            <div
+                className={`absolute ${menu ? "top-[5%]" : "top-[-100%]"} left-0 flex min-h-[30vh] w-full items-center bg-[#171d32] px-4 opacity-100 md:static md:min-h-fit md:w-auto`}
+            >
+                <ul className="flex flex-col gap-2 md:flex-row md:items-center md:gap-[2vw]">
+                    <a href="#about">
+                        <li className="hover:text-gray-500">
+                            Logout ( Username )
+                        </li>
+                    </a>
+                    {/* <a href="#contact">
+                        <li className="hover:text-gray-500">Contact</li>
+                    </a> */}
+                </ul>
+            </div>
+
+            {/* Hamburger menu icon */}
+            <div className="md:hidden">
+                <Menu
+                    className={`cursor-pointer ${menu ? "text-gray-300" : "text-white"}`}
+                    onClick={handleMenu}
+                />
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
