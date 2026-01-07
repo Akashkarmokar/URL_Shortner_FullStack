98137e5bc04a8a569c2a0d4dd02fe2957f7312e3
akash <akashcsemu@gmail.com>
2026-01-08 02:35:14 +0600

    feat: ORM and Client Has been setup

--- BackEnd/src/index.ts
+++ BackEnd/src/index.ts
@@ -3,6 +3,7 @@ import type { Express } from "express";
 import express from "express";
 import { PORT } from "./secrets.js";
 import rootRouter from "./routes/index.js";
+import { prisma } from "./lib/prisma.client";
 
 const app: Express = express();
 
