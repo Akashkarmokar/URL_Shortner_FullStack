743881e0949d36e1eafec53612872058f57776f9
akash <akashcsemu@gmail.com>
2026-01-09 00:27:45 +0600

    feat: SignIn Component has been Modified

--- FrontEnd/src/app/(auth)/signin/page.tsx
+++ FrontEnd/src/app/(auth)/signin/page.tsx
@@ -40,23 +40,12 @@ const SignIn = () => {
     return (
         <section className="bg-gray-50 dark:bg-gray-900">
             <div className="mx-auto flex flex-col items-center justify-center px-6 py-8 md:h-screen lg:py-0">
-                {/* <a
-                    href="#"
-                    className="mb-6 flex items-center text-2xl font-semibold text-gray-900 dark:text-white"
-                >
-                    <img
-                        className="mr-2 h-8 w-8"
-                        src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
-                        alt="logo"
-                    />
-                    Flowbite
-                </a> */}
                 <div className="w-full rounded-lg bg-white shadow sm:max-w-md md:mt-0 xl:p-0 dark:border dark:border-gray-700 dark:bg-gray-800">
                     <div className="space-y-4 p-6 sm:p-8 md:space-y-6">
                         <h1 className="text-xl leading-tight font-bold tracking-tight text-gray-900 md:text-2xl dark:text-white">
                             Sign in to your account
                         </h1>
-                        <form className="space-y-4 md:space-y-6" action="#">
+                        <div className="space-y-4 md:space-y-6" action="#">
                             <div>
                                 <label className="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
                                     Your email
@@ -98,7 +87,7 @@ const SignIn = () => {
                             >
                                 Sign in
                             </button>
-                        </form>
+                        </div>
                     </div>
                 </div>
             </div>
